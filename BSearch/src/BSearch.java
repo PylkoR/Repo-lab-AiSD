723e2438d1723de0ebc8456600e5caf67c196b91
Rafał <rafal.pylko@gmail.com>
2023-10-10 11:18:22 +0200

    klasa BSearch

---
+++ BSearch/src/BSearch.java
@@ -0,0 +1,22 @@
+public class BSearch {
+    public int bSearch(int[] nums, int toFind){
+        if(nums == null){
+            throw new RuntimeException("nums coś tam");
+        }
+        int p = 0;
+        int k = nums.length - 1;
+
+        while(p<=k){
+            int m = p + (k-p)/2;
+
+            if(toFind < nums[m])
+                k = m-1;
+
+            else if(toFind > nums[m])
+                p = m+1;
+            else return m;
+        }
+
+        return -1;
+    }
+}
