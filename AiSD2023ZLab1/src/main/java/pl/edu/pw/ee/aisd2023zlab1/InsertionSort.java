5a476f161b46c8e4597fdafb45a66b25fccb9f02
Rafał <rafal.pylko@gmail.com>
2023-10-17 10:58:45 +0200

    InsertionSort napisany i działa klasa testowa

--- AiSD2023ZLab1/src/main/java/pl/edu/pw/ee/aisd2023zlab1/InsertionSort.java
+++ AiSD2023ZLab1/src/main/java/pl/edu/pw/ee/aisd2023zlab1/InsertionSort.java
@@ -2,11 +2,30 @@ package pl.edu.pw.ee.aisd2023zlab1;
 
 import pl.edu.pw.ee.aisd2023zlab1.services.Sorting;
 
+import static java.util.Objects.isNull;
+
 public class InsertionSort implements Sorting {
 
     @Override
     public void sort(double[] nums) {
-        throw new UnsupportedOperationException("TODO insertion sort.");
+        validateParams(nums);
+
+        for(int i = 1; i < nums.length; i++){
+            int j = i -1;
+            double tmp = nums[i];
+
+            while(j >= 0 && tmp < nums[j]){
+                nums[j + 1] = nums[j];
+                j--;
+            }
+            nums[j+1] = tmp;
+        }
+    }
+
+    private void validateParams(double[] nums){
+        if(isNull(nums)){
+            throw new RuntimeException("Input args (nums) cannot be null!");
+        }
     }
 
 }
