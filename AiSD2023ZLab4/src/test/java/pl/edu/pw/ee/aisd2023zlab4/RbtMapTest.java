9df130eb04a2a2ed68b7bcfda6ba56bbc3c2fd20
Rafał <rafal.pylko@gmail.com>
2023-11-10 18:55:37 +0100

    testy na włąaściwości drzewa czerwono-czarnego i różne przypadki

--- AiSD2023ZLab4/src/test/java/pl/edu/pw/ee/aisd2023zlab4/RbtMapTest.java
+++ AiSD2023ZLab4/src/test/java/pl/edu/pw/ee/aisd2023zlab4/RbtMapTest.java
@@ -9,43 +9,75 @@ public class RbtMapTest {
 
     private RbtMap<Integer, String> tree;
     private boolean sameNumberOfBlackNodes;
+    private boolean noRedRightSons;
+    private boolean noTwoRedSons;
 
     @Before
     public void setup() {
         tree = new RbtMap<>();
         sameNumberOfBlackNodes = true;
+        noRedRightSons = true;
+        noTwoRedSons = true;
+    }
+
+    @Test
+    public void should_CorrectlyPutBigNumberOfNodesAndGetOne() {
+        //given
+        int size = 10_000;
+        int tryValue = 7452;
+        String tryString = "node " + tryValue;
+
+        //when
+        for (int i = 0; i < size; i++) {
+            tree.setValue(i, "node " + i);
+        }
+        String elem = tree.getValue(tryValue);
+        checkThatAllNodesDoNotHaveTwoRedSons(tree.getRootNode());
+        checkThatAllNodesDoNotHaveRedRightSon(tree.getRootNode());
+
+        //then
+        assertThat(elem).isEqualTo(tryString);
+        assertThat(tree.getRootNode().getColor()).isEqualTo(Color.BLACK);
+        assertThat(noTwoRedSons).isEqualTo(true);
+        assertThat(noRedRightSons).isEqualTo(true);
     }
 
     @Test
     public void should_makeBalancedTreeWithSameNumberOfBlackNodesInEveryPath() {
         //given
         int size = 173;
-        int numberOfBlackNodes = 1;
 
         for (int i = 0; i < size; i++) {
             tree.setValue(i, "P. Czarnek " + i);
         }
-        Node<Integer, String> tmp = tree.getRootNode();
 
         //when
-        while (tmp.getLeft() != null) {
-            if (tmp.getLeft().getColor().equals(Color.BLACK))
-                numberOfBlackNodes++;
-
-            tmp = tmp.getLeft();
-        }
-
-        tmp = tree.getRootNode();
-        countPathsLength(tmp, 0, numberOfBlackNodes);
-
+        compareNumberOfBlackNodesInEveryPath(tree.getRootNode(), 0, countLengthOfLeftBranch(tree.getRootNode()));
 
         //then
-        System.out.println(numberOfBlackNodes);
         assertThat(sameNumberOfBlackNodes).isEqualTo(true);
     }
 
     @Test
-    public void should_correctlyPutAndGetSomeValues(){
+    public void should_correctlyPutOneNodeAsARoot() {
+        //given
+        String value1 = "Ala";
+
+        //when
+        tree.setValue(1, value1);
+
+        String elem1 = tree.getValue(1);
+        String rootValue = tree.getRootNode().getValue();
+        Integer key = tree.getRootNode().getKey();
+
+        //then
+        assertThat(elem1).isEqualTo(value1);
+        assertThat(rootValue).isEqualTo(value1);
+        assertThat(key).isEqualTo(1);
+    }
+
+    @Test
+    public void should_correctlyPutAndGetSomeValues() {
         //given
         String value1 = "Ala";
         String value2 = "Ola";
@@ -70,17 +102,140 @@ public class RbtMapTest {
         assertThat(elem4).isEqualTo(value4);
     }
 
-    public void countPathsLength(Node<?, ?> root, int currentLength, int compareLength) {
-        if (root == null) {
-            if (currentLength != compareLength)
-                sameNumberOfBlackNodes = false;
+    @Test
+    public void should_CorrectlyPutNodeWithKeyThatAlreadyIsInTree() {
+        //given
+        int size = 255;
+        String swap = "P. Czarnek";
+        int keyToSwap = 177;
+
+        for (int i = 0; i < size; i++) {
+            tree.setValue(i, "Node " + i);
         }
-        else if (root.getColor().equals(Color.BLACK))
+
+        //when
+        tree.setValue(keyToSwap, swap);
+
+        String afterSwap = tree.getValue(keyToSwap);
+
+        //then
+        assertThat(afterSwap).isEqualTo(swap);
+        assertThat(tree.getRootNode().getColor()).isEqualTo(Color.BLACK);
+    }
+
+    @Test
+    public void should_AddSmallerNodeOnTheLeftSide() {
+        //given
+        String root = "root";
+        String leftSon = "leftSon";
+        int bigger = 5;
+        int smaller = bigger - 1;
+
+        //when
+        tree.setValue(bigger, root);
+        tree.setValue(smaller, leftSon);
+
+        String shouldBeLeftSon = tree.getRootNode().getLeft().getValue();
+        //then
+        assertThat(shouldBeLeftSon).isEqualTo(leftSon);
+    }
+
+    @Test
+    public void should_correctlyPutBiggerNodesAndMakeLeftRotation(){
+        //given
+        String root = "root";
+        String rightSon = "rightSon";
+        int bigger = 5;
+        int smaller = bigger - 1;
+
+        //when
+        tree.setValue(smaller, root);
+        Integer rootBeforeSet = tree.getRootNode().getKey();
+
+        tree.setValue(bigger, rightSon);
+        Integer rootAfterSet = tree.getRootNode().getKey();
+        Integer leftSonAfterSet = tree.getRootNode().getLeft().getKey();
+
+        //then
+        assertThat(rootBeforeSet).isEqualTo(smaller);
+        assertThat(rootAfterSet).isEqualTo(bigger);
+        assertThat(leftSonAfterSet).isEqualTo(smaller);
+        assertThat(tree.getRootNode().getColor()).isEqualTo(Color.BLACK);
+    }
+
+    @Test
+    public void should_correctlyPutSmallerNodesAndMakeRightRotation(){
+        //given
+        String root = "root";
+        String leftSon = "leftSon";
+        String leftGrandSon = "leftGrandSon";
+        int bigger = 5;
+        int smaller = bigger - 1;
+        int smallest = smaller - 1;
+
+        //when
+        tree.setValue(bigger, root);
+
+        tree.setValue(smaller, leftSon);
+        tree.setValue(smallest, leftGrandSon);
+        Integer rootAfterSet = tree.getRootNode().getKey();
+        Integer leftSonAfterSet = tree.getRootNode().getLeft().getKey();
+        Integer rightSonAfterSet = tree.getRootNode().getRight().getKey();
+
+        //then
+        assertThat(rootAfterSet).isEqualTo(smaller);
+        assertThat(leftSonAfterSet).isEqualTo(smallest);
+        assertThat(rightSonAfterSet).isEqualTo(bigger);
+        assertThat(tree.getRootNode().getColor()).isEqualTo(Color.BLACK);
+    }
+
+    private void checkThatAllNodesDoNotHaveRedRightSon(Node<?, ?> node){
+        if (node != null) {
+            if (node.getRight() != null && node.getRight().isRed()) {
+                    noRedRightSons = false;
+            }
+
+            checkThatAllNodesDoNotHaveTwoRedSons(node.getLeft());
+            checkThatAllNodesDoNotHaveTwoRedSons(node.getRight());
+        }
+    }
+
+    private void checkThatAllNodesDoNotHaveTwoRedSons(Node<?,?> node){
+        if (node != null) {
+            if (node.getLeft() != null && node.getRight() != null) {
+                if (node.getLeft().isRed() && node.getRight().isRed()){
+                    noTwoRedSons = false;
+                }
+            }
+
+            checkThatAllNodesDoNotHaveTwoRedSons(node.getLeft());
+            checkThatAllNodesDoNotHaveTwoRedSons(node.getRight());
+        }
+    }
+
+    private void compareNumberOfBlackNodesInEveryPath(Node<?, ?> root, int currentLength, int compareLength) {
+        if (root == null) {
+            if (currentLength != compareLength) {
+                sameNumberOfBlackNodes = false;
+            }
+        } else if (root.getColor().equals(Color.BLACK)) {
             currentLength++;
 
-        if (root != null) {
-            countPathsLength(root.getLeft(), currentLength, compareLength);
-            countPathsLength(root.getRight(), currentLength, compareLength);
+            compareNumberOfBlackNodesInEveryPath(root.getLeft(), currentLength, compareLength);
+            compareNumberOfBlackNodesInEveryPath(root.getRight(), currentLength, compareLength);
         }
+    }
+
+    private int countLengthOfLeftBranch(Node<?, ?> root) {
+        int numberOfBlackNodes = 0;
+
+        while (root != null) {
+            if (root.getColor().equals(Color.BLACK)) {
+                numberOfBlackNodes++;
+            }
+            root = root.getLeft();
+        }
+
+        return numberOfBlackNodes;
     }
 }
