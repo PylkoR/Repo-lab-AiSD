4083fc76c2104e601ca35e502690cfedc2a60705
Rafał <rafal.pylko@gmail.com>
2023-12-04 11:15:41 +0100

    jakaługa błwbłędów

--- AiSD2023ZBonus/src/main/java/pl/edu/pw/ee/aisd2023zbonus/AiSD2023ZBonus.java
+++ AiSD2023ZBonus/src/main/java/pl/edu/pw/ee/aisd2023zbonus/AiSD2023ZBonus.java
@@ -2,6 +2,9 @@ package pl.edu.pw.ee.aisd2023zbonus;
 
 public class AiSD2023ZBonus {
     public int countPalindroms(String text){
+        if (text == null || text.length() < 1){
+            throw new RuntimeException("String wejściowy jest pusty!");
+        }
         if (text.length() >= 9_999){
             return -1;
         }
@@ -10,8 +13,8 @@ public class AiSD2023ZBonus {
         for ( int i = 0; i < text.length(); i++){
             for (int j = i + 1; j <= text.length(); j++) {
 
-                String substring = text.substring(i, j);
-                if (substring.length() > 1 && isPalindrom(substring)) {
+                String subText = text.substring(i, j);
+                if (subText.length() > 1 && isPalindrom(subText)) {
                     n++;
                 }
             }
--- AiSD2023ZBonus/src/main/java/pl/edu/pw/ee/aisd2023zbonus/Main.java
+++ AiSD2023ZBonus/src/main/java/pl/edu/pw/ee/aisd2023zbonus/Main.java
@@ -3,10 +3,30 @@ package pl.edu.pw.ee.aisd2023zbonus;
 public class Main {
     public static void main(String[] args){
         String text = "KOKOO";
+        String test = "KOKON";
+        String test2 = "OOKOO";
+        String testNull = "BBB";
+        String test3 = "";
+        String test4 = "";
+        for ( int i = 0; i < 9_999; i++){
+            test3 += "A";
+        }
+        for ( int i = 0; i < 4_999; i++){
+            test4 += "A";
+        }
 
         AiSD2023ZBonus palindromCounter = new AiSD2023ZBonus();
         int n = palindromCounter.countPalindroms(text);
-
+        int k = palindromCounter.countPalindroms(test);
+        int l = palindromCounter.countPalindroms(test2);
+        int m = palindromCounter.countPalindroms(testNull);
+        int j = palindromCounter.countPalindroms(test3);
+        int h = palindromCounter.countPalindroms(test4);
         System.out.println(n);
+        System.out.println(k);
+        System.out.println(l);
+        System.out.println(m);
+        System.out.println(j);
+        System.out.println(h);
     }
 }
